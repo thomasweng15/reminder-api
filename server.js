cdf492def4a57cc1199173f7233344aba0926555
Thomas Weng <thomas.weng11@gmail.com>
2017-06-18 16:50:26 -0700

    Added api routes for creating and getting all reminders

--- server.js
+++ server.js
@@ -13,23 +13,58 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 9999;        // set our port
-
-var Reminder = require('./models/reminder');
+var port = process.env.PORT || 8080;        // set our port
 
 var mongoose = require('mongoose');
-var url = 'mongodb://localhost:27017/reminder-api';
-mongoose.connect(url); // connect to our database
+mongoose.connect('mongodb://localhost/reminder-api'); // connect to our database
 
+var Reminder = require('./models/reminder');
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/reminders')
+
+    // create a reminder (accessed at POST http://localhost:8080/api/reminders)
+    .post(function(req, res) {
+        var reminder = new Reminder();      // create a new instance of the Reminder model
+        reminder.name = req.body.name;  // set the reminder's name (comes from the request)
+
+        // save the reminder and check for errors
+        reminder.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Reminder created!' });
+        });
+    })
+
+    // get all the reminders (accessed at GET http://localhost:8080/api/reminders)
+    .get(function(req, res) {
+        Reminder.find(function(err, reminders) {
+            if (err)
+                res.send(err);
+
+            res.json(reminders);
+        });
+    });
+
 
 // more routes for our API will happen here
 
